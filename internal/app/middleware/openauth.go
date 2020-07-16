cb5723f19c263a81ccac09bd22e3e8d67bd2d26b
link-duan <itsl@foxmail.com>
2020-07-16 17:03:01 +0800

    Merge branch 'v0.2.x' of https://github.com/douyu/juno into v0.2.x
    # Conflicts:
    #	internal/app/adminengine/engine_admin.go
    #	internal/app/adminengine/engine_install.go

---
+++ internal/app/middleware/openauth.go
@@ -0,0 +1,10 @@
+package middleware
+
+import "github.com/labstack/echo/v4"
+
+func OpenAuth(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+		return next(c)
+	}
+}
