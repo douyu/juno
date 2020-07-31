0bd8eb1c620b303f246df7a36fbbbed6fed9d2bc
MEX7 <mex7.0828@gmail.com>
2020-07-31 10:14:13 +0800

    Adjust the configuration echo state logic

--- internal/pkg/service/confgov2/config.go
+++ internal/pkg/service/confgov2/config.go
@@ -9,13 +9,7 @@ import (
 	"sync"
 	"time"
 
-	"golang.org/x/sync/errgroup"
-
-	"github.com/go-resty/resty/v2"
-
 	"github.com/douyu/juno/internal/pkg/service/agent"
-	"github.com/pkg/errors"
-
 	"github.com/douyu/juno/internal/pkg/service/appevent"
 	"github.com/douyu/juno/internal/pkg/service/clientproxy"
 	"github.com/douyu/juno/internal/pkg/service/configresource"
@@ -28,22 +22,17 @@ import (
 	"github.com/douyu/juno/pkg/model/view"
 	"github.com/douyu/juno/pkg/util"
 	"github.com/douyu/jupiter/pkg/xlog"
+	"github.com/go-resty/resty/v2"
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo/v4"
-	"go.etcd.io/etcd/clientv3"
+	"github.com/pkg/errors"
 	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 )
 
 const (
-	// ServerProxyConfigurationTakeEffect ..
-	ServerProxyConfigurationTakeEffect = "/api/v1/configuration/takeEffect"
-	// ServerProxyConfigurationUsed ..
-	ServerProxyConfigurationUsed = "/api/v1/configuration/used"
-	//// QueryAgentUsingConfiguration ..
-	//QueryAgentUsingConfiguration = "/debug/config"
 	// QueryAgentUsedStatus ..
-	//QueryAgentUsedStatus = "http://%s/api/v1/conf/command_line/status"
-	QueryAgentUsedStatus = "/api/v1/conf/command_line/status"
+	queryAgentUsedStatus = "/api/v1/conf/command_line/status"
 )
 
 func List(param view.ReqListConfig) (resp view.RespListConfig, err error) {
@@ -385,186 +374,12 @@ func Instances(param view.ReqConfigInstanceList) (resp view.RespConfigInstanceLi
 	return
 }
 
-func syncUsedStatus(nodes []db.AppNode, resp []view.RespConfigInstanceItem, env, zoneCode, filePath string) ([]view.RespConfigInstanceItem, error) {
-	// get junoAgentList
-	junoAgentList := assemblyJunoAgent(nodes)
-	if len(junoAgentList) > 400 || len(junoAgentList) <= 0 {
-		return resp, errorconst.JunoAgentQueryOverSize.Error()
-	}
-	// use map
-	usedMap := make(map[string]int, 0)
-	for _, ag := range junoAgentList {
-		usedMap[ag.HostName] = getUsedStatus(env, zoneCode, filePath, ag.IPPort)
-	}
-	for k, v := range resp {
-		if resp[k].ConfigFileUsed != 0 {
-			continue
-		}
-		if newState, ok := usedMap[resp[k].HostName]; ok {
-			resp[k].ConfigFileUsed = uint(newState)
-			mysql.Model(&db.ConfigurationStatus{}).Where("id=?", v.ConfigurationStatusID).Update("used", newState)
-		}
-	}
-	return resp, nil
-}
-
-func syncPublishStatus(appName, env string, zoneCode string, configuration db.Configuration, notSyncFlag map[string]db.AppNode, resp []view.RespConfigInstanceItem) ([]view.RespConfigInstanceItem, error) {
-	for _, prefix := range cfg.Cfg.Configure.Prefixes {
-		newSyncDataMap, err := configurationSynced(appName, env, zoneCode, configuration.Name, configuration.Format, prefix, notSyncFlag)
-		if err != nil {
-			xlog.Error("syncPublishStatus", zap.String("appName", appName), zap.String("env", env), zap.String("zoneCode", zoneCode), zap.String("prefix", prefix), zap.String("err", err.Error()))
-			continue
-		}
-		xlog.Debug("syncPublishStatus", zap.String("appName", appName), zap.String("env", env), zap.String("zoneCode", zoneCode), zap.Any("newSyncDataMap", newSyncDataMap))
-
-		var version = configuration.Version
-		for k, v := range resp {
-			if resp[k].ConfigFileSynced == 1 {
-				continue
-			}
-			if newState, ok := newSyncDataMap[resp[k].HostName]; ok {
-				if newState.Version == version {
-					resp[k].ConfigFileSynced = 1
-					resp[k].Version = version
-					resp[k].ChangeLog = commitMsg(version, configuration.ID)
-					mysql.Model(&db.ConfigurationStatus{}).Where("id=?", v.ConfigurationStatusID).Update("synced", 1)
-				}
-			}
-		}
-	}
-	return resp, nil
-}
-
-func syncTakeEffectStatus(appName, governPort, env string, zoneCode string, configuration db.Configuration, notTakeEffectNodes map[string]db.AppNode, resp []view.RespConfigInstanceItem) ([]view.RespConfigInstanceItem, error) {
-	newSyncDataMap, err := configurationTakeEffect(appName, env, zoneCode, configuration.Name, configuration.Format, governPort, notTakeEffectNodes)
-	if err != nil {
-		return resp, err
-	}
-	var version = configuration.Version
-	for k, v := range resp {
-		if resp[k].ConfigFileTakeEffect == 1 {
-			continue
-		}
-		if newState, ok := newSyncDataMap[resp[k].HostName]; ok {
-			if newState.EffectVersion == version {
-				resp[k].ConfigFileTakeEffect = 1
-				mysql.Model(&db.ConfigurationStatus{}).Where("id=?", v.ConfigurationStatusID).Update("take_effect", 1)
-			}
-		}
-	}
-	return resp, nil
-}
-
-func getUsedStatus(env, zoneCode, filePath string, ipPort string) int {
-	// query proxy for used status
-	resp, err := clientproxy.ClientProxy.HttpPost(view.UniqZone{
-		Env:  env,
-		Zone: zoneCode,
-	}, view.ReqHTTPProxy{
-		Address: ipPort,
-		URL:     QueryAgentUsedStatus,
-		Params: map[string]string{
-			"config": filePath,
-		},
-	})
-	if err != nil {
-		return 0
-	}
-	configurationUsedStatus := new(struct {
-		Code int `json:"code"`
-		Data struct {
-			Supervisor bool `json:"supervisor"`
-			Systemd    bool `json:"systemd"`
-		} `json:"data"`
-		Msg string `json:"msg"`
-	})
-	if err = json.Unmarshal(resp.Body(), configurationUsedStatus); err != nil {
-		return 0
-	}
-	if configurationUsedStatus.Data.Supervisor {
-		return view.ConfigureUsedTypeSupervisor
-	}
-	if configurationUsedStatus.Data.Systemd {
-		return view.ConfigureUsedTypeSystemd
-	}
-	return 0
-}
-
 func assemblyJunoAgent(nodes []db.AppNode) []view.JunoAgent {
 	res := make([]view.JunoAgent, 0)
 	for _, node := range nodes {
 		res = append(res, view.JunoAgent{HostName: node.HostName, IPPort: node.IP + fmt.Sprintf(":%d", cfg.Cfg.Agent.Port)})
 	}
 	return res
-}
-
-func getConfigurationStatus(configurationID uint, hostName string) (res db.ConfigurationStatus, err error) {
-	query := mysql.Preload("ConfigurationPublish").Where("configuration_id=? and host_name=?", configurationID, hostName).Order("id desc", false).Find(&res)
-	if query.Error != nil {
-		err = query.Error
-		return
-	}
-	return
-}
-
-func configurationSynced(appName, env, zoneCode, filename, format, prefix string, notSyncFlag map[string]db.AppNode) (list map[string]view.ConfigurationStatus, err error) {
-	list = make(map[string]view.ConfigurationStatus, 0)
-	fileNameWithSuffix := fmt.Sprintf("%s.%s", filename, format)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	key := fmt.Sprintf("/%s/callback/%s/%s", prefix, appName, fileNameWithSuffix)
-	defer cancel()
-	resp, err := clientproxy.ClientProxy.EtcdGet(view.UniqZone{Env: env, Zone: zoneCode}, ctx, key, clientv3.WithPrefix())
-	if err != nil {
-		xlog.Warn("configurationSynced", zap.String("step", "EtcdGet"), zap.String("appName", appName), zap.String("env", env), zap.String("zoneCode", zoneCode), zap.String("key", key), zap.String("error", err.Error()))
-		return
-	}
-	if len(resp.Kvs) == 0 {
-		err = errorconst.ParamConfigCallbackKvIsZero.Error()
-		xlog.Warn("configurationSynced", zap.String("step", "resp.Kvs"), zap.String("appName", appName), zap.String("env", env), zap.String("zoneCode", zoneCode), zap.String("key", key), zap.Any("resp", resp))
-		return
-	}
-	// publish status, synced status
-	for _, item := range resp.Kvs {
-		row := view.ConfigurationStatus{}
-		if err := json.Unmarshal(item.Value, &row); err != nil {
-			continue
-		}
-		xlog.Debug("configurationSynced", zap.String("step", "for.resp.Kvs"), zap.Any("row", row), zap.Any("notSyncFlag", notSyncFlag))
-
-		if _, ok := notSyncFlag[row.Hostname]; !ok {
-			continue
-		}
-		list[row.Hostname] = row
-	}
-	xlog.Debug("configurationSynced", zap.String("step", "finish"), zap.Any("list", list))
-
-	return
-}
-
-func configurationTakeEffect(appName, env, zoneCode, filename, format, governPort string, notTakeEffectNodes map[string]db.AppNode) (list map[string]view.ConfigurationStatus, err error) {
-	list = make(map[string]view.ConfigurationStatus, 0)
-	// take effect status
-	// publish status, synced status
-	for _, node := range notTakeEffectNodes {
-		row := view.ConfigurationStatus{}
-		agentQuestResp, agentQuestError := clientproxy.ClientProxy.HttpGet(view.UniqZone{Env: env, Zone: zoneCode}, view.ReqHTTPProxy{
-			Address: node.IP + ":" + governPort,
-			URL:     cfg.Cfg.ClientProxy.HttpRouter.GovernConfig,
-		})
-		if agentQuestError != nil {
-			err = agentQuestError
-			continue
-		}
-		var out struct {
-			JunoConfigurationVersion string `json:"juno_configuration_version"`
-			JunoAgentMD5             string `json:"juno_agent_md5"`
-		}
-		_ = json.Unmarshal(agentQuestResp.Body(), &out)
-		effectVersion := out.JunoConfigurationVersion
-		row.EffectVersion = effectVersion
-		list[node.HostName] = row
-	}
-	return
 }
 
 // Publish ..
