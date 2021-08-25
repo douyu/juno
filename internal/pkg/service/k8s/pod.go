656138ad4d193361f3da8fc3cf82140ebf4775da
大可 <hnlq.sysu@gmail.com>
2021-08-25 10:55:29 +0800

    chore: improve filter logic

--- internal/pkg/service/k8s/pod.go
+++ internal/pkg/service/k8s/pod.go
@@ -107,10 +107,6 @@ func (i *syncPod) commonCheck(in *v1.Pod) error {
 		ok      bool
 	)
 
-	// 检查PodPhase
-	if in.Status.Phase != v1.PodRunning {
-		return errors.New("pod is not running")
-	}
 	// 检查appName
 	if appName, ok = in.Labels["appName"]; !ok || appName == "" {
 		return errors.New("appName is empty")
