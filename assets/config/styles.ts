fe37ecfd9f33d47fefeae0c3599cd77275450682
linthan <fsk741@gmail.com>
2021-09-06 09:41:32 +0800

    构建优化

--- assets/config/styles.ts
+++ assets/config/styles.ts
@@ -2,10 +2,6 @@ const env = process.env.NODE_ENV;
 const staticPrefix = env === 'development' ? '' : '/ant';
 
 export default [
-    // {
-    //     src: staticPrefix + '/js/codemirror/v5.62.3/lib/codemirror.css'
-    // },
-    {
-        src: staticPrefix + '/js/antd/v4.3.4/antd.css'
-    }
+    staticPrefix + '/js/codemirror/v5.62.3/lib/codemirror.css',
+    staticPrefix + '/js/antd/v4.3.4/antd.css'
 ];
