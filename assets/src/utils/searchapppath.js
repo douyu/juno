05e2028d6069a4fe9c82a68e3ce42712a51197ca
飞花(廖顶超) <feihua@dev-wh.douyu.tv>
2021-10-12 15:44:06 +0800

    历史应用跳转支持本机记录读取

--- assets/src/utils/searchapppath.js
+++ assets/src/utils/searchapppath.js
@@ -1,10 +1,17 @@
-import { getLocalForageT , setLocalForage } from './localForage';
+import { getLocalForageT, setLocalForage } from './localForage';
 
 export function setAppSearch(app, location, cb, cb2) {
-  setLocalForage(`appdefaultsearch${location.pathname}`, location.search || '', cb, cb2);
+    setLocalForage(`appdefaultsearch${location.pathname}`, location.search || '', cb, cb2);
+}
+// 按应用名设置历史操作信息（本地存储）
+export function setAppSearchByAppName(app, location, cb, cb2) {
+    setLocalForage(`appdefaultsearch-${app}-${location.pathname}`, location.search || '', cb, cb2);
 }
 
-export async function getAppSearch(app,pathname) {
-  return  await getLocalForageT(`appdefaultsearch${pathname}`);
-  }
-  
+export async function getAppSearch(app, pathname) {
+    return await getLocalForageT(`appdefaultsearch${pathname}`);
+}
+// 按应用获取历史操作信息（本地存储）
+export async function getAppSearchByAppName(app, pathname) {
+    return await getLocalForageT(`appdefaultsearch-${app}-${pathname}`);
+}
