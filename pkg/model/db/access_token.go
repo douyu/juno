ea1a6e4c4c9c8b1df641011283a264179496ed98
大可 <hnlq.sysu@gmail.com>
2022-09-05 11:00:52 +0800

    fix index name

--- pkg/model/db/access_token.go
+++ pkg/model/db/access_token.go
@@ -5,7 +5,7 @@ import "github.com/douyu/jupiter/pkg/store/gorm"
 type (
 	AccessToken struct {
 		gorm.Model
-		Name   string `json:"name" gorm:"type:varchar(32);uniqueIndex:idx_name;"`
+		Name   string `json:"name" gorm:"type:varchar(32);uniqueIndex:idx_unique_name;"`
 		AppID  string `json:"app_id" gorm:"type:varchar(32);unique;"`
 		Secret string `json:"secret" gorm:"type:varchar(64);"`
 	}
