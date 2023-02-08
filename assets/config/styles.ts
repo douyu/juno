28d5c410d8f00fcb94fd840957ca347bfbe08df4
Ethan <fsk741@gmail.com>
2023-02-08 09:29:09 +0800

    fix: config flex style (#354)
    Co-authored-by: fusikai <fusikai@douyu.tv>

--- assets/config/styles.ts
+++ assets/config/styles.ts
@@ -3,5 +3,5 @@ const staticPrefix = env === 'development' ? '' : '/ant';
 
 export default [
     staticPrefix + '/js/codemirror/v5.62.3/lib/codemirror.css',
-    staticPrefix + '/js/antd/v4.3.4/antd.css'
+    // staticPrefix + '/js/antd/v4.3.4/antd.css'
 ];
