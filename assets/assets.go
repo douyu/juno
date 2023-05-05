eeb322dcf5331a436cb5faffb2051e2d70bf652c
fusikai <fusikai@douyu.tv>
2023-05-05 14:22:09 +0800

    兼容window

--- assets/assets.go
+++ assets/assets.go
@@ -5,10 +5,12 @@ package assets
 
 import (
 	"net/http"
+	"path/filepath"
 )
 
 var AssetsEmbedded = false
 
 func Assets() (http.FileSystem, error) {
-	return http.Dir("./assets/dist"), nil
+	pathDir := filepath.Join(".", "assets", "dist")
+	return http.Dir(pathDir), nil
 }
